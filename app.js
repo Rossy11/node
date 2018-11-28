177d971d0484a5253c8d68f30323d80c74d83dbf
耿宏波 <ghb@ep-audio.com>
2018-11-28 09:30:05 +0800

    添加文件上传

--- app.js
+++ app.js
@@ -5,30 +5,56 @@ var express = require('express');
 var mysql = require('mysql');
 var app = express();
 var bodyParser = require('body-parser');
-//设置数据库
+var fs = require('fs');
+var multer = require('multer');
+var upload = multer({dest: 'uploadFile/'});
+//链接数据库
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: '192.168.1.95',
     user: 'root',
     password: '123456',
     database: 'school'
 });
 connection.connect();
 // 创建 application/x-www-form-urlencoded 编码解析(post方法)
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-//设置跨域访问
+var urlencodedParser = bodyParser.urlencoded({extended: false})
+//设置返回头
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By", "3.2.1");
     res.header("Content-Type", "application/json;charset=utf-8");
+    res.header("Content-Type", "image/jpeg");
     next();
 });
+//图片加载,存储在uploadFile下的所有图片
+app.get('/uploadFile/*', function (req, res) {
+    res.sendFile( __dirname + "/" + req.url );
+})
+//文件上传
+app.post('/upload', upload.any(), function (req, res, next) {
+    console.log(req.files[0]);  // 上传的文件信息
+    var newFile = "./uploadFile/" + req.files[0].originalname;
+    fs.readFile(req.files[0].path, function (err, data) {
+        fs.writeFile(newFile, data, function (err) {
+            if (err) {
+                console.log("错误：", err)
+            } else {
+                let response = {
+                    message: '上传成功',
+                    filename: req.files[0].originalname
+                };
+                res.json(response);
+            }
+        });
+    })
+})
+
 //登录
-app.post('/login',urlencodedParser, function (req, res) {
+app.post('/login', urlencodedParser, function (req, res) {
     var username = req.body.username;
     var password = req.body.password;
-    var sql = `select * from login where username = '${username}' and password = '${password}'`;
+    var sql = `select * from login where username = '${username}' and password = '${password}'`; //sql语句
     connection.query(sql, function (err, result) {
         console.log(result)
         if (err || result.length == 0) {
